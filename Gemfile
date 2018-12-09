34562a842dcf195b17546a4d5c6f58cd227c4bf1
shunya matsubara <dowanna@shunyanoMacBook-Pro.local>
2018-12-09 16:38:19 +0900

    user新規登録/password validation/error partial/flash/redirect/named routing

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ ruby '2.5.1'
 gem 'rails', '~> 5.2.1', '>= 5.2.1.1'
 gem 'bootstrap-sass', '3.3.7'
 gem 'bcrypt', '3.1.12'
+gem 'gravatar', '~> 1.0'
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3'
 # Use Puma as the app server
---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,43 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  test "reject invalid signupt" do
+    get signup_path
+    assert_no_difference "User.count" do
+      post signup_path, params: {
+        user: {
+          name: ' ',
+          email: 'hoge@gmailcom',
+          password: '1',
+          password_confirmation: '2'
+        }
+      }
+    end
+    assert_template 'users/new'
+    # エラーメッセージが正しい数だけ表示されていること
+    assert_select 'ul' do |e|
+      e.each do |e|
+        assert_select e, "li", 3
+      end
+    end
+    # フォームの向き先がデフォルトルートのuserではなくsignupに向いていること
+    assert_select 'form[action=?]', '/signup'
+  end
+
+  test "should add user" do
+    get signup_path
+    assert_difference "User.count", 1 do
+      post signup_path, params: {
+        user: {
+          name: 'hoge',
+          email: 'hoge@gmail.com',
+          password: 'hogehoge',
+          password_confirmation: 'hogehoge'
+        }
+      }
+    end
+    follow_redirect! #postした結果にリダイレクト
+    assert_template 'users/show'
+    assert_not flash[:success].nil?, 'flash should not be nil'
+  end
+end
